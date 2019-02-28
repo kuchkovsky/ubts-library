c5b18c406d8d71f6c474fadee33a9efe9c7229de
Yaroslav Kuchkovskyi <y.kuchkovsky@gmail.com>
2019-02-28 18:30:17 +0200

    1.0.0 release

--- src/main/java/ua/org/ubts/library/UbtsLibrary.java
+++ src/main/java/ua/org/ubts/library/UbtsLibrary.java
@@ -6,10 +6,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class LibraryApplication {
+public class UbtsLibrary {
 
     public static void main(String[] args) {
-        SpringApplication.run(LibraryApplication.class, args);
+        SpringApplication.run(UbtsLibrary.class, args);
     }
 
 }
